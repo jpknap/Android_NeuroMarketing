35f07e0e261071319c6af99ac95a496489f58f96
felipe <felipeclaude@hotmail.com>
2015-04-22 16:08:28 -0300

    Primero

---
+++ app/src/main/java/com/test/helloeeg/NotificationReceiverActivity.java
@@ -0,0 +1,16 @@
+package com.test.helloeeg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Felipe on 22/04/2015.
+ */
+
+public class NotificationReceiverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
