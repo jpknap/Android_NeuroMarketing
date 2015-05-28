611cdf12785fad4c649f5acec2dedab6466ab24a
jpknap <jp.knap@gmail.com>
2015-05-28 13:57:37 -0300

    Alarma implementada con notificacion y delicioso sonido despertador

--- app/src/main/java/com/test/helloeeg/AlarmReceiver.java
+++ app/src/main/java/com/test/helloeeg/AlarmReceiver.java
@@ -3,6 +3,9 @@ package com.test.helloeeg;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -11,7 +14,15 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         // For our recurring task, we'll just display a message
-        Toast.makeText(context, "NOTIFICACION", Toast.LENGTH_SHORT).show();
+
+        Intent notificationIntent = new Intent(context,NotificacionAlarma.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(notificationIntent);
+
+
+
+
+        // Toast.makeText(context, "NOTIFICACION", Toast.LENGTH_SHORT).show();
 
     }
 }
---
+++ app/src/main/java/com/test/helloeeg/NotificacionAlarma.java
@@ -0,0 +1,55 @@
+package com.test.helloeeg;
+
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class NotificacionAlarma extends ActionBarActivity {
+
+    Ringtone r;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificacion_alarma);
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            r = RingtoneManager.getRingtone(this, notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notificacion_alarma, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void pararAlarma(View view) {
+        r.stop();
+
+    }
+}
